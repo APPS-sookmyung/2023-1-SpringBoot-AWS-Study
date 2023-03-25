61110d01fd0138f4fa0c8ca7a4d14ab739ac5e24
5jisoo <56earls@gmail.com>
2023-03-26 00:15:49 +0900

    Feat: spring data jpa 실습 코드
    - 실습 코드

---
+++ 오지수/springboot-aws/src/test/java/com/apps/springbootaws/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,65 @@
+package com.apps.springbootaws.domain.posts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional  // 테스트 완료 후 항상 롤백해줌.
+class PostsRepositoryTest {
+    @Autowired
+    PostsRepository postsRepository;
+
+    @Test
+    public void 게시글저장_불러오기() throws Exception {
+        // given
+        String title = "테스트 게시글";
+        String content = "게시글 본문";
+
+        postsRepository.save(
+                Posts.builder()
+                        .title(title)
+                        .content(content)
+                        .author("earls@gmail.com")
+                        .build()
+        );
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() throws Exception {
+        // given
+        LocalDateTime now = LocalDateTime.of(2023, 3, 26, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+
+        System.out.println(">>>>>>>> createDate=" + posts.getCreatedDate()
+                + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+
+}
---
+++ 오지수/springboot-aws/src/test/java/com/apps/springbootaws/web/PostsApiControllerTest.java
@@ -0,0 +1,104 @@
+package com.apps.springbootaws.web;
+
+import com.apps.springbootaws.domain.posts.Posts;
+import com.apps.springbootaws.domain.posts.PostsRepository;
+import com.apps.springbootaws.web.dto.PostsSaveRequestDto;
+import com.apps.springbootaws.web.dto.PostsUpdateRequestDto;
+import org.aspectj.lang.annotation.After;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+import static org.springframework.http.HttpStatus.*;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+// @Transactional 붙여도 롤백이 되지 않음. RAMDOM_PORT는 별도의 스레드에서 스프링 컨테이너를 실행시키기 때문.
+class PostsApiControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;  // HTTP 요청 후 데이터를 응답 받을 수 있는 테믈릿 객체.
+
+    @Autowired
+    private PostsRepository postsRepository;
+
+    @AfterEach  // @After과 같은 애노테이션.
+    public void tearDown() throws Exception {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void Posts_등록된다() throws Exception {
+        // given
+        String title = "title";
+        String content = "content";
+        PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+
+        String url = "http://localhost" + port + "/api/v1/posts";
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate.postForEntity(url, requestDto, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void Posts_수정() throws Exception {
+        // given
+        Posts savedPost = postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long id = savedPost.getId();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + id;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        // when
+        ResponseEntity<Long> responseEntity = restTemplate
+                .exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        // then
+        assertThat(responseEntity.getStatusCode()).isEqualTo(OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> all = postsRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+    }
+}
