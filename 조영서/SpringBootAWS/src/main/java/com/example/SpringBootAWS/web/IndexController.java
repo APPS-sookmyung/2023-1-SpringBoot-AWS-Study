ac4e9f4b5d3a0fa29459cd8fa9ce55cd0dcca618
youngseo cho <dudtj0412@naver.com>
2023-04-05 23:18:10 +0900

    Feat: 3주차 실습 코드

---
+++ 조영서/SpringBootAWS/src/main/java/com/example/SpringBootAWS/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.example.SpringBootAWS.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        return "posts-save";
+    }
+
+}
