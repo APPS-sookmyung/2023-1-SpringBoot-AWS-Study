5cd7cb31f4af1927704716b0af405b627957d829
5jisoo <56earls@gmail.com>
2023-05-03 16:52:32 +0900

    Refactor: 어노테이션 기반으로 개선

--- 오지수/springboot-aws/src/main/java/com/apps/springbootaws/web/IndexController.java
+++ 오지수/springboot-aws/src/main/java/com/apps/springbootaws/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.apps.springbootaws.web;
 
+import com.apps.springbootaws.config.auth.LoginUser;
 import com.apps.springbootaws.config.auth.dto.SessionUser;
 import com.apps.springbootaws.service.posts.PostsService;
 import com.apps.springbootaws.web.dto.PostsResponseDto;
@@ -18,9 +19,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
