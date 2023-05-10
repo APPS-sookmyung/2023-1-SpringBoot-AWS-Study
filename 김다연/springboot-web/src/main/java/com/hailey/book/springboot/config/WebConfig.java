f4e78a60245829de503e8e1c7f6f2c16f7b24eae
HaileyKim <rlaekdus17@sookmyung.ac.kr>
2023-05-10 14:39:08 +0900

    Feat:[5주차 스프링 스터디] 김다연

--- 김다연/springboot-web/src/main/java/com/hailey/book/springboot/config/WebConfig.java
+++ 김다연/springboot-web/src/main/java/com/hailey/book/springboot/config/WebConfig.java
@@ -11,11 +11,11 @@ import java.util.List;
 
 @RequiredArgsConstructor
 @Configuration
-public class WebConfig implements WebMvcConfigurer{
+public class WebConfig implements WebMvcConfigurer {
     private final LoginUserArgumentResolver loginUserArgumentResolver;
 
     @Override
-    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers){
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(loginUserArgumentResolver);
     }
 }
